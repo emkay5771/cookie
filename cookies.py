3c345fbfc47351d75d029ca22f0c939bec1bde03
Mendy Kay <52011243+emkay5771@users.noreply.github.com>
2024-03-17 22:41:11 +0000

    add recipe

---
+++ cookies.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(page_title="Cookies", page_icon=":cookie:")
+
+st.title("Cookie Recipe 🍪")
+
+"""Best Ever Chocolate Chip Cookie Recipe
+
+INGREDIENTS
+
+1 1/4 cups (165g) all purpose flour, spooned and leveled
+1/2 tsp baking soda
+1/2 tsp baking powder
+3/4 tsp salt
+1/2 cup (110g) unsalted butter, melted and cooled
+1/2 cup (100g) light brown sugar, packed
+1/3 cup (75g) white sugar
+2 tsp vanilla extract
+1 large egg
+1 1/2 cups (240g) semisweet chocolate chips + more for topping
+Flaky sea salt
+
+INSTRUCTIONS
+
+Preheat the oven to 350F and line a large baking sheet with parchment paper.
+In a medium sized bowl, whisk together the flour (see notes on how to properly measure), baking soda, baking powder, and salt. Set aside.
+In a large mixing bowl, add the melted and cooled butter, brown sugar, and sugar. It’s important that the butter is not hot, but it can be slightly warm. Whisk vigorously for 1-2 minutes until it turns into a paste-like consistency. Whisk in the egg and vanilla until smooth. Add the baking soda, baking powder and salt - mix to combine. Add in the flour and use a rubber spatula to fold to combine. Leave a few streaks of flour and pour in the chocolate chips. Continue to fold the dough until the chocolate chips are dispersed and the flour is completely blended in. Do not over mix.
+A large 2oz cookie scoop (or 1/4 measuring cup) is preferred for this recipe. Scoop 5 cookies onto the baking sheet and bake for 12-15 minutes (12 for a more gooey cookie, 15 more a more well done cookie). If you do not have a large scoop or prefer regular-sized cookies, scoop 6 cookies onto the baking sheet and bake for 10-12 minutes.
+The cookies are ready when they have a golden ring around the edge but are pale and puffed in the center. And if your cookies spread too much, use a fork to nudge in the sides immediately after the cookies are pulled from the oven. Sprinkle with flaky salt and let cool 20 minutes (on a rack) before enjoying."""
